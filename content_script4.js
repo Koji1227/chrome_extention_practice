8fc146fe9480ed28b72eba21cdf3f68a735a495a
Koji1227 <115434297+Koji1227@users.noreply.github.com>
2022-12-31 00:38:21 +0900

    Change extention

---
+++ content_script4.js
@@ -0,0 +1,9 @@
+#related{
+    
+}
+
+// let related = document.querySelectorAll(".list-rst__calendar-frame");
+// for(let i = 0; i < calendars.length; i++){
+//   let e = calendars[i];
+//   e.style.display = 'none';
+// }
